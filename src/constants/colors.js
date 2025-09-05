6c1d95e8d0d21a0dedc28ffc6e3dce13bf1966ce
Benjamin Kratiroff <benji.kratiroff@gmail.com>
2025-09-04 19:29:57 -0700

    maps and navigation tab

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  primary: 'dodgerblue',
+  secondary: '#FFF',
+  accent: 'tomato',
+};
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+const MapScreen = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 48.8566,
+          longitude: 2.3522,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default MapScreen;
